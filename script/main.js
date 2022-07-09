891b98a7e8f3996eaf6b97d0fed3af2130676d64
Ula <ursz.kam@gmail.com>
2022-07-10 00:27:17 +0200

    style: fix confirmation message

--- script/main.js
+++ script/main.js
@@ -7,7 +7,7 @@ xhr.send();
 xhr.getLocation = function(checkboxElem) {
     if (checkboxElem.checked) {
         let response = JSON.parse(this.responseText);
-        let insertCountry = confirm(`Do you wan to put down ${response.country} as your country`);
+        let insertCountry = confirm(`Do you wan to put down ${response.country} as your country?`);
         if (insertCountry === true) {
             document.getElementById('country').defaultValue = response.country;
         }
