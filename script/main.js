84435066d2875c270e91fbdffca916200e384f9b
Ula <ursz.kam@gmail.com>
2022-07-10 00:21:32 +0200

    fix: insert location from IP as default value

--- script/main.js
+++ script/main.js
@@ -1,16 +1,19 @@
-const endpoint = 'http://ip-api.com/json/?fields=status,message,country,query';
+const ip_endpoint = 'http://ip-api.com/json/?fields=status,message,country,query';
 const xhr = new XMLHttpRequest();
+
+xhr.open('GET', ip_endpoint, true);
+xhr.send();
 
 xhr.getLocation = function(checkboxElem) {
     if (checkboxElem.checked) {
         let response = JSON.parse(this.responseText);
         let insertCountry = confirm(`Do you wan to put down ${response.country} as your country`);
-        if (insertCountry == true) {
-            return response.country
+        if (insertCountry === true) {
+            document.getElementById('country').defaultValue = response.country;
         }
-        if (response.status != 'success') {
+        if (response.status !== 'success') {
             alert(`We aren't able to identify your location.`)
         }
-    xhr.open('GET', endpoint, true);
-    xhr.send();
+    }
 }
+
