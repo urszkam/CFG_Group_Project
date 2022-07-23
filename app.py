138d45b6e67776eb34487b813436b4b1c55e60c8
urszkam <ursz.kam@gmail.com>
2022-07-23 23:40:26 +0200

    feat: get movie data from API

--- app.py
+++ app.py
@@ -3,65 +3,107 @@
 # from importlib.util import module_for_loader
 import requests
 import pprint
+import pandas as pd
+import json
 
 # app = Flask(__name__, template_folder='templates', static_url_path='/static')
 
 
 # def formDetails():
 #     title = request.form.get('title')
-#     country = request.form.get('country')
+#     country = request.form.get('country').capitalize()
 #     return (title, country)
 
+pp = pprint.PrettyPrinter(indent=4)
 
-def searchNetflix(title):
+movie_type = "series"
+country = 'Spain'
+title = 'Breaking Bad'
+
+headers = {
+    "X-RapidAPI-Key": "710918eac5msh667cd317416a88dp15c140jsnaeb4f681bd95",
+    "X-RapidAPI-Host": "unogs-unogs-v1.p.rapidapi.com"
+}
+
+
+def search_netflix(title,type,headers):
     url = "https://unogs-unogs-v1.p.rapidapi.com/search/titles"
-
-    querystring = {"order_by":"date","title":{title},"type":"series"}
-
-    headers = {
-        "X-RapidAPI-Key": "710918eac5msh667cd317416a88dp15c140jsnaeb4f681bd95",
-        "X-RapidAPI-Host": "unogs-unogs-v1.p.rapidapi.com"
-    }
+    querystring = {"order_by":"date","title":{title},"type":{type}}
 
     response = requests.get(url, headers=headers, params=querystring)
     search_results = response.json()
 
     return search_results
 
+
+def read_countries():
+    df = pd.read_csv('countries.csv', usecols=[0,1,2])
+    return df
+
+
 def getWordsInTitle(movie):
     title_lower = movie['title'].lower().strip()
     words_in_title = set(title_lower.split(" "))
     return words_in_title
 
-pp = pprint.PrettyPrinter(indent=4)
 
-# pp.pprint(searchNetflix("breaking bad"))
-title = 'Breaking Bad'
-title = title.lower().strip()
-words_in_title = set(title.split(" "))
-print(words_in_title)
+def movies_list(search_results,title):
+    title = title.lower().strip()
+    words_in_title = set(title.split(" "))
+    movies_list = search_results['results']
+    exact_titles = [movie for movie in movies_list if movie['title'].lower().strip() == title]
 
-response = searchNetflix("Breaking Bad")
-# pp.pprint(response['results'])
+    best_ids = set()
+    for movie in exact_titles:
+        best_ids.add(movie['netflix_id'])
+
+    similar_titles = [movie for movie in movies_list if len(getWordsInTitle(movie).intersection(words_in_title)) > 0]
+    similar_titles = [movie for movie in similar_titles if movie['netflix_id'] not in best_ids]
+    exact_titles.extend(similar_titles)
+    return exact_titles
+
+def get_countries(id, headers):
+    url = "https://unogs-unogs-v1.p.rapidapi.com/title/countries"
+    querystring = {"netflix_id":id}
+
+    response = requests.get(url, headers=headers, params=querystring)
+    search_results = response.json()
+
+    return search_results['results']
+
+def actors(id):
+    url = "https://unogs-unogs-v1.p.rapidapi.com/search/people"
+    querystring = {"netflix_id":id,"person_type":"Actor","limit":"10"}
+
+    response = requests.get(url, headers=headers, params=querystring)
+    search_results = response.json()
+    actors_list = []
+    for actor in search_results['results']:
+        actors_list.append(actor['full_name'])
+    return actors_list
+
+
+def country_data(countries, country):
+    df_countries = pd.DataFrame(countries)
+    countries_num = len(df_countries.index)-1
+    df_countries['country'] = df_countries['country'].str.strip()
+    chosen_country_data = df_countries.loc[df_countries['country'] == country]
+    every_country = df_countries['country'].tolist()
+    other_countries = [x for x in every_country if x != country]
+    return (chosen_country_data, countries_num, other_countries)
+
+response = search_netflix("Breaking Bad",movie_type,headers)
+movies_list = movies_list(response,title)
+pp.pprint(movies_list[0])
+
+id = movies_list[0]['netflix_id']
+countries = get_countries(id, headers)
+
+chosen_country_data, countries_num, other_countries_list = country_data(countries,country)
+actors_list = actors(id)
 
 
 
-movies_list = response['results']
-
-exact_titles = [movie for movie in movies_list if movie['title'].lower().strip() == title]
-
-best_ids = set()
-for movie in exact_titles:
-    best_ids.add(movie['netflix_id'])
-
-print(best_ids)
-similar_titles = [movie for movie in movies_list if len(getWordsInTitle(movie).intersection(words_in_title)) > 0]
-
-similar_titles = [movie for movie in similar_titles if movie['netflix_id'] not in best_ids]
-exact_titles.extend(similar_titles)
-
-for s in exact_titles:
-    pp.pprint(s['title'])
 
 # @app.route('/', methods=['GET', 'POST'])
 # def index():
@@ -75,3 +117,4 @@ for s in exact_titles:
 #     app.run(debug=True)
 
 
+# data to return: countries_cum, other_countries_list, actors_list
