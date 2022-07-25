8d674a5c7edf7741411a9134b565cd0612ac9050
Ula <ursz.kam@gmail.com>
2022-07-25 02:51:41 +0200

    feat: add right push menu

--- script/main.js
+++ script/main.js
@@ -17,3 +17,25 @@ xhr.getLocation = function(checkboxElem) {
     }
 }
 
+$(function($) {
+    var openBtn = $("#open-button"),
+      colseBtn = $("#close-button"),
+      menu = $(".menu-wrap");
+    // Open menu when click on menu button
+    openBtn.on("click", function() {
+      menu.addClass("active");
+    });
+    // Close menu when click on Close button
+    colseBtn.on("click", function() {
+      menu.removeClass("active");
+    });
+    // Close menu when click on anywhere on the document
+    $(document).on("click", function(e) {
+      var target = $(e.target);
+      if (target.is(".menu-wrap, .menu, .icon-list, .icon-list a, .icon-list a span, #open-button") === false) {
+        menu.removeClass("active");
+        e.stopPropagation();
+      }
+    });
+  })(jQuery);
+  
